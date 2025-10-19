07db1b327f45d1a91d762fe3f9975afb6686bfcb
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-19 17:04:05 +0200

    Add comment to project view model

---
+++ ADE-WFM/Models/ViewModels/CommentViewModels/AddCommentProjectViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ADE_WFM.Models.ViewModels.CommentViewModels
+{
+    public class AddCommentProjectViewModel
+    {
+        public int ProjectId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public Comment Comment { get; set; } = new Comment();
+    }
+}
