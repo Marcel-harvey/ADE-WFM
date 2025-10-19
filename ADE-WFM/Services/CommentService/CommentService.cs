e8442cf15389808c197069c80339744deb0b1d72
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-19 16:13:54 +0200

    Added add comment service

---
+++ ADE-WFM/Services/CommentService/CommentService.cs
@@ -0,0 +1,40 @@
+﻿using ADE_WFM.Models.ViewModels.CommentViewModels;
+using ADE_WFM.Models;
+using Microsoft.EntityFrameworkCore;
+using ADE_WFM.Data;
+
+namespace ADE_WFM.Services.CommentService
+{
+    public class CommentService : ICommentService
+    {
+        private readonly ApplicationDbContext _context;
+        public CommentService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET serivces
+
+        // UPDATE services
+
+        // ADD services
+        // Add comment to workflow
+        public async Task AddCommentToWorkFlow(AddCommentWorkFlowViewModel model)
+        {
+            var comment = new Comment
+            {
+                DateCreated = DateOnly.FromDateTime(DateTime.UtcNow),
+                CommentContent = model.Comment.CommentContent,
+                UserId = model.Comment.User.Id,
+                WorkFlowId = model.Comment.WorkFlowId,
+                IsViewed = false,
+            };
+
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+        }
+
+
+        // DELETE services
+    }
+}
---
+++ ADE-WFM/Services/CommentService/ICommentService.cs
@@ -0,0 +1,16 @@
+﻿using ADE_WFM.Models.ViewModels.CommentViewModels;
+
+namespace ADE_WFM.Services.CommentService
+{
+    public interface ICommentService
+    {
+        // GET serivces
+
+        // UPDATE services
+
+        // ADD services
+        Task AddCommentToWorkFlow(AddCommentWorkFlowViewModel model);
+
+        // DELETE services
+    }
+}
