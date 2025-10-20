d76bc9ab96ca1b44f73491294691d56dc81a4185
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-20 10:29:27 +0200

    Database migrations

--- ADE-WFM/Migrations/20251020082642_AddedCascadeDeleteForProject.Designer.cs
+++ ADE-WFM/Migrations/20251020082642_AddedCascadeDeleteForProject.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ADE_WFM.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ADE_WFM.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251020082642_AddedCascadeDeleteForProject")]
+    partial class AddedCascadeDeleteForProject
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ADE-WFM/Migrations/20251020082642_AddedCascadeDeleteForProject.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ADE_WFM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCascadeDeleteForProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Projects_ProjectId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_WorkFlows_WorkFlowId",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskPlanning_Projects_ProjectId",
+                table: "TaskPlanning");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "TaskPlanning",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "TaskPlanning",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskPlanning_UserId",
+                table: "TaskPlanning",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Projects_ProjectId",
+                table: "Comments",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_WorkFlows_WorkFlowId",
+                table: "Projects",
+                column: "WorkFlowId",
+                principalTable: "WorkFlows",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskPlanning_AspNetUsers_UserId",
+                table: "TaskPlanning",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskPlanning_Projects_ProjectId",
+                table: "TaskPlanning",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Projects_ProjectId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_WorkFlows_WorkFlowId",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskPlanning_AspNetUsers_UserId",
+                table: "TaskPlanning");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskPlanning_Projects_ProjectId",
+                table: "TaskPlanning");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskPlanning_UserId",
+                table: "TaskPlanning");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TaskPlanning");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "TaskPlanning",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Projects_ProjectId",
+                table: "Comments",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_WorkFlows_WorkFlowId",
+                table: "Projects",
+                column: "WorkFlowId",
+                principalTable: "WorkFlows",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskPlanning_Projects_ProjectId",
+                table: "TaskPlanning",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+        }
+    }
+}
