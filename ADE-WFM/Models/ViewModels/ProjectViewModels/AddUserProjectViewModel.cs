1f7bece459eb3f157ccce64fea32037de3e3c7e2
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-20 09:54:39 +0200

    Add user to project API service

---
+++ ADE-WFM/Models/ViewModels/ProjectViewModels/AddUserProjectViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ADE_WFM.Models.ViewModels.ProjectViewModels
+{
+    public class AddUserProjectViewModel
+    {
+        public int ProjectId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+    }
+}
