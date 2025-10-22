f093b58d51b32e5ea5b2e1fd1b9ee1d5896ad92f
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-22 13:51:05 +0200

    Added workflow controller with get API

---
+++ ADE-WFM/Controllers/WorkFlowController.cs
@@ -0,0 +1,25 @@
+﻿using ADE_WFM.Services.WorkFlowService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADE_WFM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkFlowController : ControllerBase
+    {
+        private readonly IWorkFlowService _workFlowService;
+        public WorkFlowController(IWorkFlowService workFlowService)
+        {
+            _workFlowService = workFlowService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var workflows = await _workFlowService.GetAllWorkFlows();
+            return Ok(workflows);
+        }
+    }
+}
