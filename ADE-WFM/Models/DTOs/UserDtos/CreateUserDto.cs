1ca792b5ab241c2dc443362de6812ed735b8f291
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-23 18:57:03 +0200

    Added fields to dto

--- ADE-WFM/Models/DTOs/UserDtos/CreateUserDto.cs
+++ ADE-WFM/Models/DTOs/UserDtos/CreateUserDto.cs
@@ -2,5 +2,12 @@
 {
     public class CreateUserDto
     {
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;    
+        public string? Password { get; set; }
+        // For multiple errors using Identity roles
+        public IEnumerable<string>? Roles { get; set; } 
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
