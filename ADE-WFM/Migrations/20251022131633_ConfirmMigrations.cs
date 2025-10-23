437651ba3a981458772ac1c32d209d94825e4d91
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-23 08:47:52 +0200

    Confirmed migrations

---
+++ ADE-WFM/Migrations/20251022131633_ConfirmMigrations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ADE_WFM.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfirmMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
