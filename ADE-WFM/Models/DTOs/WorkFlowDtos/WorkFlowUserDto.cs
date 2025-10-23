d4b21357235374647eb9459dc4b88ef62b00c769
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-23 09:13:10 +0200

    Added user dto for responses

---
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/WorkFlowUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ADE_WFM.Models.DTOs.WorkFlowDtos
+{
+    public class WorkFlowUserDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
