510d0a8d58b097978b49a63bbb4147aae6ac957f
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-23 11:13:27 +0200

    Added remove user dto

---
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/RemoveUserFromWorkFlowDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ADE_WFM.Models.DTOs.WorkFlowDtos
+{
+    public class RemoveUserFromWorkFlowDto
+    {
+        public string UserId { get; set; } = string.Empty;
+        public int WorkFlowId { get; set; }
+    }
+}
