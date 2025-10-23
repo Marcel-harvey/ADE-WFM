55cf4030e41392dc990ff3931b566abaf4ac53a9
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-23 09:13:25 +0200

    Added response for adding new user

---
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/ResponseAddUserWorkFlowDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ADE_WFM.Models.DTOs.WorkFlowDtos
+{
+    public class ResponseAddUserWorkFlowDto
+    {
+        public int WorkFlowId { get; set; }
+        public string WorkFlowName { get; set; } = string.Empty;
+        public List<WorkFlowUserDto> Users { get; set; } = new();
+        public string Message { get; set; } = string.Empty;
+    }
+}
