b8ed096d24119ae3f0dcd6e37495b7df5890b2e7
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-23 18:57:15 +0200

    Added fields to response dto

--- ADE-WFM/Models/DTOs/UserDtos/ResponseCreateUserDto.cs
+++ ADE-WFM/Models/DTOs/UserDtos/ResponseCreateUserDto.cs
@@ -2,5 +2,9 @@
 {
     public class ResponseCreateUserDto
     {
+        public bool Succeeded { get; set; }
+        public string? UserId { get; set; }
+        public IEnumerable<string> Errors { get; set; } = Array.Empty<string>();
+        public string? TemporaryPassword { get; set; }
     }
 }
