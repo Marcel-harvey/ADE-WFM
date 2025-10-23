4674233ee85a903cf58843360e276c81428ad251
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-23 09:37:22 +0200

    Added response for updating work flow name

---
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/ResponseUpdateWorkFlowNameDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ADE_WFM.Models.DTOs.WorkFlowDtos
+{
+    public class ResponseUpdateWorkFlowNameDto
+    {
+        public string OldName { get; set; } = string.Empty;
+        public string NewName { get; set; } = string.Empty;
+    }
+}
