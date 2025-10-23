f9a5f037a138afe851d139805becf5d7703da48f
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-23 08:49:03 +0200

    Changed namespace name to dto not view model

--- ADE-WFM/Models/DTOs/WorkFlowDtos/AddUserWorkFlowDto.cs
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/AddUserWorkFlowDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace ADE_WFM.Models.DTOs.WorkFlowViewModels
 {
-    public class AddUserWorkFlowViewModel
+    public class AddUserWorkFlowDto
     {
         public List<string> UserIds { get; set; } = new();
         public int WorkFlowId { get; set; }
