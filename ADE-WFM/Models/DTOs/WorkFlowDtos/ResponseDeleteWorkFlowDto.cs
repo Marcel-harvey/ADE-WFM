8007511622bd0654b3c4981be7ad9665f6bb95e3
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-23 10:05:26 +0200

    Added response for deleting work flow

---
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/ResponseDeleteWorkFlowDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ADE_WFM.Models.DTOs.WorkFlowDtos
+{
+    public class ResponseDeleteWorkFlowDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
