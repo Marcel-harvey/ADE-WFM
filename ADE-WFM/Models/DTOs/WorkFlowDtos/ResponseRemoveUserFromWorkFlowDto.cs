4634a2d333f8a0512de17e71aa5f0bf294e1e5c1
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-23 11:13:41 +0200

    Added response dto for removing user

---
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/ResponseRemoveUserFromWorkFlowDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ADE_WFM.Models.DTOs.WorkFlowDtos
+{
+    public class ResponseRemoveUserFromWorkFlowDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
