118ebaa0565e034ed4518043f19994114363890a
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-23 08:50:39 +0200

    Added response for creating new workflow

---
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/ResponseCreateWorkFlowDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ADE_WFM.Models.DTOs.WorkFlowDtos
+{
+    public class ResponseCreateWorkFlowDto
+    {
+        public int Id { get; set; }
+        public string WorkFlowName { get; set; } = string.Empty;
+        public string CreatedByUserId { get; set; } = string.Empty;
+        public List<string> AssignedUserIds { get; set; } = new();
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string Message { get; set; } = "Workflow created successfully";
+    }
+}
