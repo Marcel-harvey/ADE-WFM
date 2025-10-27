9ecc02c51e14e17a6a92863a46caa3a0241012c4
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-27 12:24:57 +0200

    Added dto for getting users in projects

---
+++ ADE-WFM/Models/DTOs/ProjectDtos/GetProjectUsersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ADE_WFM.Models.DTOs.ProjectDtos
+{
+    public class GetProjectUsersDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
