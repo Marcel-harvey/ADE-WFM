2a3d106615985b35a48c066ca703503886c43297
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-27 12:24:42 +0200

    Added DTO for get by id request

---
+++ ADE-WFM/Models/DTOs/ProjectDtos/GetProjectByIdDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ADE_WFM.Models.DTOs.ProjectDtos
+{
+    public class GetProjectByIdDto
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ ADE-WFM/Models/DTOs/ProjectDtos/GetProjectByIdResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using ADE_WFM.Models.DTOs.WorkFlowDtos;
+
+namespace ADE_WFM.Models.DTOs.ProjectDtos
+{
+    public class GetProjectByIdResponseDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateOnly DateCreated { get; set; }
+        public DateOnly DueDate { get; set; }
+
+        // Lists
+        public List<GetProjectUsersDto>? Users { get; set; }
+
+        // Counts
+        public int UserCount => Users?.Count ?? 0;
+    }
+}
