70aabea35e71ecbbd9a454f99a1b94643ec649b1
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-27 12:25:12 +0200

    Created response dto for creating new project

---
+++ ADE-WFM/Models/DTOs/ProjectDtos/CreateProjectResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ADE_WFM.Models.DTOs.ProjectDtos
+{
+    public class CreateProjectResponseDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DueDate { get; set; }
+        public List<string> AssignedUserIds { get; set; } = new();
+    }
+}
