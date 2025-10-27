610d60b8c352b6f5e04b85e7b35e93f05db27bcd
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-27 09:58:56 +0200

    Renamed DTO

--- ADE-WFM/Models/DTOs/WorkFlowDtos/RemoveUserFromWorkFlowResponseDto.cs
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/RemoveUserFromWorkFlowResponseDto.cs
@@ -1,8 +1,7 @@
 ﻿namespace ADE_WFM.Models.DTOs.WorkFlowDtos
 {
-    public class ResponseRemoveUserFromWorkFlowDto
+    public class RemoveUserFromWorkFlowResponseDto
     {
         public string Name { get; set; } = string.Empty;
-        public string Message { get; set; } = string.Empty;
     }
 }
