330b6034290c518a57e654ec92091994821185f9
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-27 12:23:43 +0200

    Added get by id and create controllers

--- ADE-WFM/Controllers/ProjectController.cs
+++ ADE-WFM/Controllers/ProjectController.cs
@@ -1,5 +1,6 @@
 ﻿using ADE_WFM.Models.DTOs;
 using ADE_WFM.Models.DTOs.ProjectDtos;
+using ADE_WFM.Models.DTOs.WorkFlowDtos;
 using ADE_WFM.Services.ProjectService;
 using ADE_WFM.Services.WorkFlowService;
 using Microsoft.AspNetCore.Http;
@@ -17,6 +18,59 @@ namespace ADE_WFM.Controllers
         {
             _projectService = projectService;
         }
+
+
+        // CREATE API's
+        // Create a new projects
+        [HttpPost("Create-new")]
+        public async Task<IActionResult> CreatePorject([FromBody] CreateProjectDto dto)
+        {
+            try
+            {
+                var result = await _projectService.CreateProject(dto);
+
+                if (!result.Succeeded)
+                    return BadRequest(result);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ServiceResult<CreateProjectResponseDto>.Failure(
+                    "An unexpected error occurred while creating the project.",
+                    new[] { ex.Message }
+                ));
+            }
+        }
+
+
+        // GET API's
+        // Get project by Id
+        [HttpGet("Get/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var dto = new GetProjectByIdDto { Id = id };
+                var result = await _projectService.GetProjectById(dto);
+
+                if (!result.Succeeded)
+                    return NotFound(result);
+
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ServiceResult<GetProjectByIdResponseDto>.Failure(
+                    "An unexpected error occurred while retrieving the project.",
+                    new[] { ex.Message }
+                ));
+            }
+        }
+
+        // UPDATE API's
+
+        // DELETE API's
 
     }
 }
