632083881f9997cb0c82b02cd1cf2e336398bf31
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-27 10:30:10 +0200

    Added controller for projects with scaffolding

---
+++ ADE-WFM/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using ADE_WFM.Models.DTOs;
+using ADE_WFM.Models.DTOs.ProjectDtos;
+using ADE_WFM.Services.ProjectService;
+using ADE_WFM.Services.WorkFlowService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADE_WFM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+    }
+}
