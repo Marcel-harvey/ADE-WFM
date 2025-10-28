5efb4cc1d1b6f2b5ed63c22bdd6032e4777620c9
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-28 07:42:26 +0200

    Removed duplicate Dto

--- ADE-WFM/Models/DTOs/ProjectDtos/ProjectUserInfoDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ADE_WFM.Models.DTOs.ProjectDtos
-{
-    public class ProjectUserInfoDto
-    {
-        public string UserId { get; set; } = string.Empty;
-        public string UserName { get; set; } = string.Empty;
-    }
-
-}
