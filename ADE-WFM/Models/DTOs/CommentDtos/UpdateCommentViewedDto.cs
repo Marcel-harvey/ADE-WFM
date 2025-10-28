4ae8b6d47a23fe8a3db33f6af5d3a2b9cb898ca8
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-28 21:09:56 +0200

    Added DTO for marking comments as viewed/not viewed

---
+++ ADE-WFM/Models/DTOs/CommentDtos/UpdateCommentViewedDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ADE_WFM.Models.DTOs.CommentDtos
+{
+    public class UpdateCommentViewedDto
+    {
+        public int CommentId { get; set; }
+        public bool IsViewed { get; set; }
+    }
+}
---
+++ ADE-WFM/Models/DTOs/CommentDtos/UpdateCommentViewedResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ADE_WFM.Models.DTOs.CommentDtos
+{
+    public class UpdateCommentViewedResponseDto
+    {
+        public int CommentId { get; set; }
+        public bool IsViewed { get; set; } = true;
+    }
+}
