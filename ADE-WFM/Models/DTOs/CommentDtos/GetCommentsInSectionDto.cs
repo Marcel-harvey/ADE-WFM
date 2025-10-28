cb8a192b61bb4bbfc005cd24f2d8eb5bd77a7f78
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-28 12:28:36 +0200

    Created DTO's for getting comments in a section (WorFlow, projects etc.)

---
+++ ADE-WFM/Models/DTOs/CommentDtos/GetCommentsInSectionDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ADE_WFM.Models.DTOs.CommentDtos
+{
+    public class GetCommentsInSectionDto
+    {
+        public int WorkFlowId { get; set; }
+    }
+}
---
+++ ADE-WFM/Models/DTOs/CommentDtos/GetCommentsResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ADE_WFM.Models.DTOs.CommentDtos
+{
+    public class GetCommentsResponseDto
+    {
+        // DTO is used for response in Get users, Get projects, Get workflows, Get comments APIs
+        public int CommentId { get; set; }
+        public string CommentContent { get; set; } = string.Empty;
+        public DateOnly DateCreated { get; set; }
+        public string? WorkFlowName { get; set; }
+        public string? ProjectTitle { get; set; }
+        public string? UserName { get; set; }
+    }
+}
