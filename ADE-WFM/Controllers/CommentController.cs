90e844799c9764691ada609ad827e7f5fb588be8
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-28 11:44:02 +0200

    Added comment controller with scaffolding

---
+++ ADE-WFM/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using ADE_WFM.Models.DTOs;
+using ADE_WFM.Models.DTOs.ProjectDtos;
+using ADE_WFM.Models.DTOs.WorkFlowDtos;
+using ADE_WFM.Services.CommentService;
+using ADE_WFM.Services.ProjectService;
+using ADE_WFM.Services.WorkFlowService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace ADE_WFM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+    }
+}
