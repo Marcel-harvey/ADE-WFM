76bf0c7e61e67c90ea0d6c59ec0f4da36676699e
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-29 14:43:48 +0200

    Added DTO's for showing and getting info

---
+++ ADE-WFM/Models/DTOs/TodoDtos/GetToDoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ADE_WFM.Models.DTOs.TodoDtos
+{
+    public class GetToDoDto
+    {
+        public int ToDoId { get; set; }
+        public string? UserId { get; set; }
+        public string? ProjectId { get; set; }
+    }
+}
---
+++ ADE-WFM/Models/DTOs/TodoDtos/ToDoResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ADE_WFM.Models.DTOs.TodoDtos
+{
+    public class ToDoResponseDto
+    {
+        public int Id { get; set; }
+        public bool IsComplete { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime DateCreated { get; set; }
+        public DateTime DueDate { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public int? ProjectId { get; set; }
+    }
+}
