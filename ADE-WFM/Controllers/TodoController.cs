85ef5bdc5b19b825a66ad9fe452ae297d3e28ae2
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-29 15:08:53 +0200

    Added todo controller with scaffolding

---
+++ ADE-WFM/Controllers/TodoController.cs
@@ -0,0 +1,21 @@
+﻿using ADE_WFM.Models.DTOs;
+using ADE_WFM.Models.DTOs.TodoDtos;
+using ADE_WFM.Services.TodoService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ADE_WFM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoService _todoService;
+        public TodoController(ITodoService todoService)
+        {
+            _todoService = todoService;
+        }
+    }
+}
