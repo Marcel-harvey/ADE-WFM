eca8b97b8dec2e09dbb63470fc1fe51e218f7ffd
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-29 10:14:17 +0200

    Added sticky note controller with scaffolding

---
+++ ADE-WFM/Controllers/StickyNoteController.cs
@@ -0,0 +1,21 @@
+﻿using ADE_WFM.Models.DTOs;
+using ADE_WFM.Models.DTOs.StickyNoteDto;
+using ADE_WFM.Services.StickyNoteService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADE_WFM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StickyNoteController : ControllerBase
+    {
+        private readonly IStickyNoteService _stickyNoteService;
+        public StickyNoteController(IStickyNoteService stickyNoteService)
+        {
+            _stickyNoteService = stickyNoteService;
+        }
+
+    }
+}
