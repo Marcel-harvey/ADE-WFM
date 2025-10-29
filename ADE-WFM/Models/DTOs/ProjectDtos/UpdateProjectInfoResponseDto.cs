d1a8bc675d242e38e0f49934ef66d77bda0e5567
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-29 08:43:49 +0200

    Removed duplicate field

--- ADE-WFM/Models/DTOs/ProjectDtos/UpdateProjectInfoResponseDto.cs
+++ ADE-WFM/Models/DTOs/ProjectDtos/UpdateProjectInfoResponseDto.cs
@@ -2,7 +2,6 @@
 {
     public class UpdateProjectInfoResponseDto
     {
-        public string ProjectTitle { get; set; } = string.Empty;
         public string? Title { get; set; }
         public string? Description { get; set; }
         public DateOnly? DueDate { get; set; }
