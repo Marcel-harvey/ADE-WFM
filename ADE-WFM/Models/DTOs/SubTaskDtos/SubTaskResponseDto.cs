1994732214e944726d1a6bfea94e184b246cb5ab
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-30 09:05:00 +0200

    Added uniform DTO for response

---
+++ ADE-WFM/Models/DTOs/SubTaskDtos/SubTaskResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ADE_WFM.Models.DTOs.SubTaskDtos
+{
+    public class SubTaskResponseDto
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+
+        public string? TodoTitle { get; set; } = string.Empty;
+
+    }
+}
