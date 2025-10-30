bc0401281cbb019b44cb3baa5a3deace887a1e75
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-30 09:07:38 +0200

    Added general get for sub task

---
+++ ADE-WFM/Models/DTOs/SubTaskDtos/GetSubTasksDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ADE_WFM.Models.DTOs.SubTaskDtos
+{
+    public class GetSubTasksDto
+    {
+        public int? SubTaskId { get; set; }
+        public string? UserId { get; set; } = string.Empty;
+    }
+}
