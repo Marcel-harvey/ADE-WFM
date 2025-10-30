1942db9d4d3509734761a58f04a435474036c37e
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-30 08:31:33 +0200

    Added sub task controller with scaffolding

---
+++ ADE-WFM/Controllers/SubTaskController.cs
@@ -0,0 +1,20 @@
+﻿using ADE_WFM.Models.DTOs;
+using ADE_WFM.Models.DTOs.SubTaskDtos;
+using ADE_WFM.Services.SubTaskService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADE_WFM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubTaskController : ControllerBase
+    {
+        private readonly ISubTaskService _subTaskService;
+        public SubTaskController(ISubTaskService subTaskService)
+        {
+            _subTaskService = subTaskService;
+        }
+    }
+}
