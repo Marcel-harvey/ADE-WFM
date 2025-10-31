6cfe8f8eda5731b7f1c92d36c7e3984c3d5f7521
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-31 08:51:43 +0200

    Created generic response DTO for projects

---
+++ ADE-WFM/Models/DTOs/ProjectDtos/ProjectResponseDto.cs
@@ -0,0 +1,25 @@
+﻿namespace ADE_WFM.Models.DTOs.ProjectDtos
+{
+    public class ProjectResponseDto
+    {
+        public int ProjectId { get; set; }
+        public string ProjectTitle { get; set; } = string.Empty;
+        public string? ProjectDescription { get; set; }
+        public DateOnly DateCreated { get; set; }
+        public DateOnly DueDate { get; set; }
+        public int WorkFlowId { get; set; }
+        public string WorkFlowName { get; set; } = string.Empty;
+
+
+        // Lists
+        public List<ProjectUsersInfoDto>? Users { get; set; }
+        public List<ProjectCommentsInfoDto>? Comments { get; set; }
+        public List<ProjectTodosInfoDto>? Todos { get; set; }
+
+        // Counts
+        public int UserCount => Users?.Count ?? 0;
+        public int CommentCount => Comments?.Count ?? 0;
+        public int TodoCount => Todos?.Count ?? 0;
+
+    }
+}
