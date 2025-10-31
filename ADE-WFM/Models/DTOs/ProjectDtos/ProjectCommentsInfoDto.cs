1d406cc695a704938c222f92300e2c46e5358cfb
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-31 08:52:03 +0200

    Created DTO's that are linked to project response DTO

---
+++ ADE-WFM/Models/DTOs/ProjectDtos/ProjectCommentsInfoDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ADE_WFM.Models.DTOs.ProjectDtos
+{
+    public class ProjectCommentsInfoDto
+    {
+        public int CommentId { get; set; }
+        public string CommentContent { get; set; } = string.Empty;
+        public DateOnly DateCreated { get; set; }
+        public bool IsViewed { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
---
+++ ADE-WFM/Models/DTOs/ProjectDtos/ProjectTodosInfoDto.cs
@@ -0,0 +1,22 @@
+﻿namespace ADE_WFM.Models.DTOs.ProjectDtos
+{
+    public class ProjectTodosInfoDto
+    {
+        public int TodoId { get; set; }
+        public bool TodoIsComplete { get; set; }
+        public string TodoTitle { get; set; } = string.Empty;
+        public string TodoDescription { get; set; } = string.Empty;
+        public DateTime DateCreated { get; set; }
+        public DateTime DueDate { get; set; }
+        public string UserName { get; set; } = string.Empty;
+
+        public List<ProjectTodoSubTasksInfoDto> ProjectTodoSubTasks { get; set; } = new();
+    }
+
+    public class ProjectTodoSubTasksInfoDto
+    {
+        public int SubTaskId { get; set; }
+        public string SubTaskDescription { get; set; } = string.Empty;
+        public bool SubTaskIsCompleted { get; set; }
+    }
+}
