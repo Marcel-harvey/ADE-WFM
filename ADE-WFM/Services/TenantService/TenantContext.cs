a1336b55c4e4273fa82e45868436b99202460500
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-03 15:47:04 +0200

    added tenant context

---
+++ ADE-WFM/Services/TenantService/TenantContext.cs
@@ -0,0 +1,15 @@
+﻿namespace ADE_WFM.Services.TenantService
+{
+    // Request Scoped Service for tenants
+    public class TenantContext
+    {
+        public int? TenantId { get; private set; }
+        public string? TenantName { get; private set; }
+
+        public void SetTenant(int id, string name)
+        {
+            TenantId = id;
+            TenantName = name;
+        }
+    }
+}
