778e5a38ed70d2d4cf19965acab36848ab762543
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-03 15:27:29 +0200

    Added db seeder for seeding tenant on app start up

---
+++ ADE-WFM/Data/DbSeeder.cs
@@ -0,0 +1,17 @@
+﻿using ADE_WFM.Models;
+
+namespace ADE_WFM.Data
+{
+    public static class DbSeeder
+    {
+        public static async Task SeedAsync(ApplicationDbContext ctx, ILogger logger)
+        {
+            if (!ctx.Tenants.Any())
+            {
+                ctx.Tenants.Add(new Tenant { Name = "Default", Domain = "default" });
+                await ctx.SaveChangesAsync();
+                logger.LogInformation("Seeded default tenant.");
+            }
+        }
+    }
+}
