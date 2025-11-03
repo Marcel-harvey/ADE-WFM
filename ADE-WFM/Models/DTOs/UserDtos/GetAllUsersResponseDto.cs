17e363479be65b35de5b94550673c500692bc58e
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-03 08:27:53 +0200

    Removed un needed DTO

--- ADE-WFM/Models/DTOs/UserDtos/GetAllUsersResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ADE_WFM.Models.DTOs.UserDtos
-{
-    public class GetAllUsersResponseDto
-    {
-        public string Id { get; set; } = string.Empty;
-        public string UserName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-    }
-}
