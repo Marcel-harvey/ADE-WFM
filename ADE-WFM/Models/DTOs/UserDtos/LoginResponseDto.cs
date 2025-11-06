a1af9f8ca4c6bf133491ffcc461ac173945538e7
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-06 14:03:14 +0200

    Added DTO for loggin in user

---
+++ ADE-WFM/Models/DTOs/UserDtos/LoginResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ADE_WFM.Models.DTOs.UserDtos
+{
+    public class LoginResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public int TenantId { get; set; }
+        public string TenantName { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
