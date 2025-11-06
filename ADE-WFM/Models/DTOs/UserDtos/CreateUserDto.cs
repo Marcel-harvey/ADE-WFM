946c81bfe8b9c87fb440c02c2c2fbf1b024f2ca6
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-06 12:56:37 +0200

    Added tenant fields for creating a new user with tenant

--- ADE-WFM/Models/DTOs/UserDtos/CreateUserDto.cs
+++ ADE-WFM/Models/DTOs/UserDtos/CreateUserDto.cs
@@ -5,7 +5,10 @@
         public string UserName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;    
         public string? Password { get; set; }
-        // For multiple errors using Identity roles
+        public string TenantName { get; set; } = string.Empty;
+        public string? TenantDomain { get; set; }
+        public string? TenantConnectionString { get; set; }
+
         public IEnumerable<string>? Roles { get; set; } 
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
