e8eeb10ea707744884ccbf2d0d1d895980ef8e4a
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-06 15:47:42 +0200

    Removed UserId field

--- ADE-WFM/Models/DTOs/WorkFlowDtos/CreateWorkFlowDto.cs
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/CreateWorkFlowDto.cs
@@ -3,7 +3,6 @@
     public class CreateWorkFlowDto
     {
         public string WorkFlowName { get; set; } = string.Empty;
-        public string CurrentUserId { get; set; } = string.Empty;
         public List<string> UserIds { get; set; } = new();
     }
 }
