c5440c8fe0307b90a60c73754c4aa095a3dbbea7
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-06 18:57:16 +0200

    Removed DTO on get user comments

--- ADE-WFM/Services/CommentService/ICommentService.cs
+++ ADE-WFM/Services/CommentService/ICommentService.cs
@@ -13,7 +13,7 @@ namespace ADE_WFM.Services.CommentService
         // GET serivces
         Task<ServiceResult<List<CommentResponseDto>>> GetWorkFlowComments(GetCommentInfoDto dto);
         Task <ServiceResult<List<CommentResponseDto>>> GetProjectComments(GetCommentInfoDto dto);
-        Task<ServiceResult<List<CommentResponseDto>>> GetUserComments(GetCommentInfoDto dto);
+        Task<ServiceResult<List<CommentResponseDto>>> GetUserComments();
 
         // UPDATE services
         Task<ServiceResult<CommentResponseDto>> MarkCommentAsViewed(UpdateCommentViewedDto dto);
