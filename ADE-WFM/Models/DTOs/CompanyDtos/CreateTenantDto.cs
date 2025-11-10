c1dde7e878382f2afd43be19aef0acadcd214c36
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-10 22:05:09 +0200

    Added Create DTO

---
+++ ADE-WFM/Models/DTOs/CompanyDtos/CreateTenantDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ADE_WFM.Models.DTOs.CompanyDtos
+{
+    public class CreateTenantDto
+    {
+        public string CompanyName { get; set; } = string.Empty;
+        public string? ConnetctionString { get; set; }
+        public string? Domain { get; set; }
+    }
+}
