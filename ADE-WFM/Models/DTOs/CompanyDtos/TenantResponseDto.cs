5b48d603236e17a25e22f887e174c9b932b1842f
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-10 22:05:22 +0200

    Added Response DTO

---
+++ ADE-WFM/Models/DTOs/CompanyDtos/TenantResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ADE_WFM.Models.DTOs.CompanyDtos
+{
+    public class TenantResponseDto
+    {
+        public int TenantId { get; set; }
+        public string CompanyName { get; set; } = string.Empty;
+        public string CreatedUser { get; set; } = string.Empty;
+    }
+}
