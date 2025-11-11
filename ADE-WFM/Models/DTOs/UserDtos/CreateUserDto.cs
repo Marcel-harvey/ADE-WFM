107b9bcc2cc8c81f724ce6911429c55ae4049bf5
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-11 10:28:25 +0200

    Added delete service

--- ADE-WFM/Models/DTOs/UserDtos/CreateUserDto.cs
+++ ADE-WFM/Models/DTOs/UserDtos/CreateUserDto.cs
@@ -5,12 +5,13 @@
         public string UserName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
-        public string? TenantToken { get; set; }
+        public int? TenantId {  get; set; }
+        public Guid? TenantToken { get; set; }
         //public string TenantName { get; set; } = string.Empty;
         //public string? TenantDomain { get; set; }
         //public string? TenantConnectionString { get; set; }
 
-        public IEnumerable<string>? Roles { get; set; } 
+        public string Role { get; set; } = string.Empty;
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
     }
