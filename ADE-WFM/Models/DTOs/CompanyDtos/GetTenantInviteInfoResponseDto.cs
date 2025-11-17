6c83721790a36d2c4c4fedd08e759974dcf50416
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-17 13:32:08 +0200

    Added feature: Used in place of the response that accept invite gave, as it can be called before accepting the invite and then used to display the information to the front end

---
+++ ADE-WFM/Models/DTOs/CompanyDtos/GetTenantInviteInfoResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ADE_WFM.Models.DTOs.CompanyDtos {
+    public class GetTenantInviteInfoResponseDto {
+        public Guid TenantToken { get; set; }
+        public string UserEmail { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+    }
+}
