05c23654b9b1d11d508d9152e604409e4d0a8afe
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-18 12:09:13 +0200

    Migration: Added title to sticky note model

---
+++ ADE-WFM/Migrations/20251118100840_AddedTitleToStickyNote.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ADE_WFM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTitleToStickyNote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "StickyNotes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "StickyNotes");
+        }
+    }
+}
