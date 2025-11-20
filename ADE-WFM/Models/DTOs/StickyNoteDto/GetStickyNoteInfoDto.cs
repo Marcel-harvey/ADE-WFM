7b3c9ab00b5f41e9ce008d4211b5e1aa1e50364c
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-20 13:53:00 +0200

    Added field: Added title field for get info DTO

--- ADE-WFM/Models/DTOs/StickyNoteDto/GetStickyNoteInfoDto.cs
+++ ADE-WFM/Models/DTOs/StickyNoteDto/GetStickyNoteInfoDto.cs
@@ -1,6 +1,7 @@
 ﻿namespace ADE_WFM.Models.DTOs.StickyNoteDto {
     public class GetStickyNoteInfoDto {
         public int StickyNoteId { get; set; }
+        public string NewTitle { get; set; } = string.Empty;
         public string NewContent { get; set; } = string.Empty;
     }
 }
