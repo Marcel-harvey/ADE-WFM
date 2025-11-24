185c7d781c6a6a5aea3237eba3f2ee9939336c56
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-24 19:54:05 +0200

    Added field: Description field added for giving a description of what the workflow is for

--- ADE-WFM/Models/DTOs/WorkFlowDtos/CreateWorkFlowDto.cs
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/CreateWorkFlowDto.cs
@@ -1,6 +1,7 @@
 ﻿namespace ADE_WFM.Models.DTOs.WorkFlowDtos {
     public class CreateWorkFlowDto {
         public string WorkFlowName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public List<string> UserIds { get; set; } = new();
     }
 }
