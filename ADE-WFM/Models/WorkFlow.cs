1fa41e212de1c9d443c9146b3f39ea0b7fbeb8cf
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-24 11:00:17 +0200

    Added feature: Added created user that created the workflow and date created

--- ADE-WFM/Models/WorkFlow.cs
+++ ADE-WFM/Models/WorkFlow.cs
@@ -7,6 +7,8 @@ namespace ADE_WFM.Models {
         public int Id { get; set; }
         [Required]
         public string WorkFlowName { get; set; } = string.Empty;
+        public string userCreated { get; set; } = string.Empty;
+        public DateTime dateCreated { get; set; } = DateTime.UtcNow;
 
 
         // Navigation Properties
