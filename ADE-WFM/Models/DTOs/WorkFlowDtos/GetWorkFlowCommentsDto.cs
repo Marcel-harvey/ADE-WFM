bc32beaaf10a32fa33acb39fd98f3f29cf84fda0
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-24 19:54:49 +0200

    Added feature: DTO used for getting comment information and displaying it on the workflow response DTO

---
+++ ADE-WFM/Models/DTOs/WorkFlowDtos/GetWorkFlowCommentsDto.cs
@@ -0,0 +1,6 @@
+﻿namespace ADE_WFM.Models.DTOs.WorkFlowDtos {
+    public class GetWorkFlowCommentsDto {
+        public int Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+    }
+}
