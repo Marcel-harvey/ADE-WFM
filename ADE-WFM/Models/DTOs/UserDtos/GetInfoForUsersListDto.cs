87a5a72a0e5f7c8da81d7e46f5b40c1a3696d228
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-26 11:39:34 +0200

    Added DTO: Get DTO for getting information for the user list requested, will be used for getting program id and project id

---
+++ ADE-WFM/Models/DTOs/UserDtos/GetInfoForUsersListDto.cs
@@ -0,0 +1,6 @@
+﻿namespace ADE_WFM.Models.DTOs.UserDtos {
+    public class GetInfoForUsersListDto {
+        public int ProgramId { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
