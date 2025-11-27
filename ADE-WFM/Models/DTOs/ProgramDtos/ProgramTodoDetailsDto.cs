8779f96a91d20af10d8c7fb6f971707880e69e05
Marcel Harvey <marcelharvey1@gmail.com>
2025-11-27 11:42:19 +0200

    Added DTO: For todo per project - wil link to master display selected program

---
+++ ADE-WFM/Models/DTOs/ProgramDtos/ProgramTodoDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ADE_WFM.Models.DTOs.ProgramDtos {
+    public class ProgramTodoDetailsDto {
+        public int TodoId { get; set; }
+        public bool isComplete { get; set; }
+        public string Task { get; set; } = string.Empty;
+        public DateOnly DateCreated { get; set; }
+        public DateOnly DueDate { get; set; }
+    }
+}
