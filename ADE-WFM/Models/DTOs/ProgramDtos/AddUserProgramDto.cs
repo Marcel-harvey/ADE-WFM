e852a74c2a77a4decf3c531905fcbf6506cb3c3e
Marcel Harvey <marcelharvey1@gmail.com>
2025-12-03 12:54:35 +0200

    Renamed field: Renamed field in DTO from workflowId to programId

--- ADE-WFM/Models/DTOs/ProgramDtos/AddUserProgramDto.cs
+++ ADE-WFM/Models/DTOs/ProgramDtos/AddUserProgramDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace ADE_WFM.Models.DTOs.WorkFlowViewModels {
     public class AddUserProgramDto {
         public List<string> UserIds { get; set; } = new();
-        public int WorkFlowId { get; set; }
+        public int programId { get; set; }
     }
 }
