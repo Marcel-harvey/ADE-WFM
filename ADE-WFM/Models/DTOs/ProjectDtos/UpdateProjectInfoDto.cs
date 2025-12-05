69925f4bd4029b8104aa84c481d530c2fdd00cb3
Marcel Harvey <marcelharvey1@gmail.com>
2025-12-05 22:05:07 +0200

    Changed field name in update project dto

--- ADE-WFM/Models/DTOs/ProjectDtos/UpdateProjectInfoDto.cs
+++ ADE-WFM/Models/DTOs/ProjectDtos/UpdateProjectInfoDto.cs
@@ -1,7 +1,7 @@
 ﻿namespace ADE_WFM.Models.DTOs.ProjectDtos {
     public class UpdateProjectInfoDto {
         public int ProjectId { get; set; }
-        public string? Title { get; set; }
+        public string? projectTitle { get; set; }
         public string? Description { get; set; }
         public DateOnly? DueDate { get; set; }
     }
