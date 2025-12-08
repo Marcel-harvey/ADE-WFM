0049afffb9f04109a24c61b0ac9aa582b65eda5b
Marcel Harvey <marcelharvey1@gmail.com>
2025-12-08 11:06:51 +0200

    Changed DTO for update todo to better take required information

--- ADE-WFM/Models/DTOs/TodoDtos/UpdateTodoDto.cs
+++ ADE-WFM/Models/DTOs/TodoDtos/UpdateTodoDto.cs
@@ -1,8 +1,9 @@
 ﻿namespace ADE_WFM.Models.DTOs.TodoDtos {
     public class UpdateTodoDto {
         public int TodoId { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public DateTime DueDate { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Task { get; set; } = string.Empty;
+        public DateOnly DueDate { get; set; }
+        public bool? IsComplete { get; set; }
     }
 }
