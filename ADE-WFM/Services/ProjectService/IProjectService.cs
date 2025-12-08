6c5cd8e4e42ffea23bafed4e8b99ddcc3f744354
Marcel Harvey <marcelharvey1@gmail.com>
2025-12-08 20:32:51 +0200

    Changed response DTO on add users to project to just display user information

--- ADE-WFM/Services/ProjectService/IProjectService.cs
+++ ADE-WFM/Services/ProjectService/IProjectService.cs
@@ -7,7 +7,7 @@ namespace ADE_WFM.Services.ProjectService {
         // ADD services
         Task<ServiceResult<ProjectResponseDto>> CreateProject(CreateProjectDto dto);
         // TODO: Convert to batch add
-        Task<ServiceResult<ProjectResponseDto>> AddUserToProject(AddUserToProjectDto dto);
+        Task<ServiceResult<List<ProjectUsersInfoDto>>> AddUserToProject(AddUserToProjectDto dto);
 
         // GET services
         Task<ServiceResult<List<ProjectResponseDto>>> GetAllProjects();
