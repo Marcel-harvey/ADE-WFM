8aa0a9bda6bf89269deddab7a786200d8e37bb23
Marcel Harvey <marcelharvey1@gmail.com>
2025-12-09 08:28:01 +0200

    Added project DTO: Response DTO for when new users is added, returns list of all current users part of the project

---
+++ ADE-WFM/Models/DTOs/ProjectDtos/UpdateProjectUserResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ADE_WFM.Models.DTOs.ProjectDtos {
+    public class UpdateProjectUserResponseDto {
+        public int? ProjectId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+    }
+}
