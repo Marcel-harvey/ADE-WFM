97ac113c4a1b254b716c8cc9df796f14e0559a8c
Marcel Harvey <marcelharvey1@gmail.com>
2025-12-13 22:45:12 +0200

    Change field names in DTO
    - Suits the rest of the DTO's namming convention to standardize for front end

--- ADE-WFM/Models/DTOs/TodoDtos/ToDoResponseDto.cs
+++ ADE-WFM/Models/DTOs/TodoDtos/ToDoResponseDto.cs
@@ -1,11 +1,10 @@
 ﻿namespace ADE_WFM.Models.DTOs.TodoDtos {
     public class ToDoResponseDto {
-        public int Id { get; set; }
+        public int todoId { get; set; }
         public bool IsComplete { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public DateTime DateCreated { get; set; }
-        public DateTime DueDate { get; set; }
+        public string Task { get; set; } = string.Empty;
+        public DateOnly DateCreated { get; set; }
+        public DateOnly DueDate { get; set; }
         public string UserName { get; set; } = string.Empty;
         public int? ProjectId { get; set; }
         public List<TodoSubTasksResponseDto> SubTasks { get; set; } = new();
