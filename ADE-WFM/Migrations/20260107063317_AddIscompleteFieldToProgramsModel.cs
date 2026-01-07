94a7d964bf693972ea4e5de67746d1b8c72525b0
Marcel Harvey <marcelharvey1@gmail.com>
2026-01-07 14:36:35 +0200

    Add field to program model
    - Add is complete field to program model
    - Field will be used to mark a program is complete

---
+++ ADE-WFM/Migrations/20260107063317_AddIscompleteFieldToProgramsModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ADE_WFM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIscompleteFieldToProgramsModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Iscompleted",
+                table: "Programs",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Iscompleted",
+                table: "Programs");
+        }
+    }
+}
