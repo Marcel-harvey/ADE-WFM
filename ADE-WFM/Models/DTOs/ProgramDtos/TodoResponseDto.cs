5f2b9b4e72a276e90f67282957c082e97c1b4aec
Marcel Harvey <marcelharvey1@gmail.com>
2026-01-10 22:21:19 +0200

    Add all fields to todo response DTO
    - DTO requires more fields to send all the todo information to the frontend
    - Add all required fields to the response DTO

--- ADE-WFM/Models/DTOs/ProgramDtos/TodoResponseDto.cs
+++ ADE-WFM/Models/DTOs/ProgramDtos/TodoResponseDto.cs
@@ -1,5 +1,9 @@
 ﻿namespace ADE_WFM.Models.DTOs.ProgramDtos {
     public class TodoResponseDto {
+        public int todoId { get; set; }
         public string Task { get; set; } = string.Empty;
+        public DateOnly DateCreated { get; set; }
+        public DateOnly? DueDate { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
